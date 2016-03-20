5bdc075cb4b809997e546494208123351f8b4378
ehsan7677 <ehsankhoshi76@gmail.com>
2016-03-20 12:43:15 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,6 +1,6 @@
-package.path = package.path .. ';.luarocks/share/lua/5.2/?.lua'
-  ..';.luarocks/share/lua/5.2/?/init.lua'
-package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
+package.path = package.path .. ';.luarocks/share/lua/5.1/?.lua'
+  ..';.luarocks/share/lua/5.1/?/init.lua'
+package.cpath = package.cpath .. ';.luarocks/lib/lua/5.1/?.so'
 
 require("./bot/utils")
 
