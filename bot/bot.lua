9e363eccb380113d9bed5153dae570e522d3d98c
ehsan7677 <ehsankhoshi76@gmail.com>
2016-03-20 10:00:57 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {155942468},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
